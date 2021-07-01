1cfc2d94f92fad435e7cb1b497cfd786cf62985e
dhiego-gomes <dhiego.jw@gmail.com>
2021-06-30 22:59:41 -0300

    :rocket: Envia 'Middlewares'

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -7,6 +7,19 @@ app.use(express.json())
 
 const customers = []
 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers
+    const customer = customers.find(customer => customer.cpf === cpf)
+
+    if (!customer) {
+        return response.status(400).json({error: 'Customer not found.'})
+    }
+
+    request.customer = customer
+
+    return next()
+}
+
 app.post('/account', (request, response) => {
     const { cpf, name } = request.body
 
@@ -26,6 +39,12 @@ app.post('/account', (request, response) => {
     })
 
     return response.status(201).send()
+})
+
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request
+    
+    return response.json(customer.statement) 
 })
 
 app.listen(3333)
